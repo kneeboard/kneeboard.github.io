a7038069ae87da226ab669477209ec60479b7c30
Kneeboard <kneeboardnotes@outlook.com>
2023-08-12 13:35:29 +0100

    Header update

--- web-d6b6d8d3a63a3e2f.js
+++ web-d6b6d8d3a63a3e2f.js
@@ -745,7 +745,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('web-f1c1abf0bd8b5ac5_bg.wasm', import.meta.url);
+        input = new URL('web-d6b6d8d3a63a3e2f_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
